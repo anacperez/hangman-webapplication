762c85d152b3059fa4301dce31facb0df4b90de8
anaperez <anaperez@csumb.edu>
2016-09-26 22:31:26 -0700

    Added acceptance test

--- src/test/java/feature/webclient/HangmanWebClientStep.java
+++ src/test/java/feature/webclient/HangmanWebClientStep.java
@@ -1,8 +1,5 @@
 package feature.webclient;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -10,42 +7,41 @@ import cucumber.api.java.en.When;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 public class HangmanWebClientStep {
-	 WebDriver driver = new ChromeDriver();
-
-	    @After
-	    public void tearDown() {
-	        driver.quit(); // close browser
-	    }
-
-	    @Given("^I am on the (\\w+) page$")
-	    public void i_am_on_the_hello_page(String pageName) throws Throwable {
-	        driver.get("http://localhost:8080/" + pageName + ".html");
-	    }
-
-	    @When("^I enter \"([^\"]*)\"$")
-	    public void i_enter(String newGuesses) throws Throwable {
-	        driver.findElement(By.id("newGuesses")).sendKeys(newGuesses);
-	        driver.findElement(By.id("submit")).click();
-	    }
-
-	    @Then("^I should see the result: \"([^\"]*)\"$")
-	    public void i_see_the_greeting(String result) throws Throwable {
-	        // The greeting is rendered dynamically with JavaScript.
-	        // Wait for the page to load, timeout after 10 seconds
-	        (new WebDriverWait(driver, 10))
-	            .until(ExpectedConditions.textToBe(By.id("pattern"), result));
-	    }
-
-	    @Then("^I see an error message$")
-	    public void i_see_an_error_message() throws Throwable {
-	        // The greeting is rendered dynamically with JavaScript.
-	        // Wait for the page to load, timeout after 10 seconds
-	    	(new WebDriverWait(driver, 10))
-            .until(ExpectedConditions.textToBePresentInElementLocated(By.id("pattern"), ""));
-	    }
+    WebDriver driver = new ChromeDriver();
+    
+    @After
+    public void tearDown() {
+        driver.quit(); // close browser
+    }
+    
+    @Given("^I am on the (\\w+) page$")
+    public void i_am_on_the_hangman_page(String name) throws Throwable {
+        driver.get("http://localhost:8080/" +name + ".html");
+    }
+    
+    @When("^I enter \"([^\"]*)\"$")
+    public void i_enter(String newGuesses) throws Throwable {
+        driver.findElement(By.id("newGuesses")).sendKeys(newGuesses);
+        driver.findElement(By.id("submit")).click();
+    }
+    
+    @Then("^I should see this result \"([^\"]*)\"$")
+    public void i_should_see_this_result(String result) throws Throwable {
+        // The greeting is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10))
+        .until(ExpectedConditions.textToBe(By.id("pattern"), result));
+    }
+    
+    @Then("^I should see nothing occur$")
+    public void i_see_nothing_occur() throws Throwable {
+        // The greeting is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10))
+        .until(ExpectedConditions.textToBePresentInElementLocated(By.id("pattern"), ""));
+    }
 }
